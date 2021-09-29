738c604de59ef665cc32c715edf703aaa919f41d
pmgvspernik@gmail.com <pmgvspernik@gmail.com>
2021-09-29 09:05:00 +0300

    Add project

---
+++ Zadacha 4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Zadacha_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(' ').ToArray();
+            Array.Reverse(arr);
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
