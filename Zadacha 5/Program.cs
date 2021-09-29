3bec9c2cb117fd9a79827a150e5482b051d99921
pmgvspernik@gmail.com <pmgvspernik@gmail.com>
2021-09-29 09:17:05 +0300

    Add project

---
+++ Zadacha 5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Zadacha_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                if (currentNumber % 2 == 0)
+                {
+                    evenSum += currentNumber;
+                }
+                else
+                {
+                    oddSum += currentNumber;
+                }
+            }
+
+            int difference = evenSum - oddSum;
+            Console.WriteLine(difference);
+
+            
+            
+        }    
+    }
+}
