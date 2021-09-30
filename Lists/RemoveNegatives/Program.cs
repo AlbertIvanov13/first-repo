c18de6ec89a569289fd316e15ba38fc00426c459
pmgvspernik@gmail.com <pmgvspernik@gmail.com>
2021-09-30 12:48:39 +0300

    Add project

---
+++ Lists/RemoveNegatives/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace RemoveNegatives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).Reverse().ToList();
+
+            numbers.RemoveAll(n => n < 0);
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join("  ", numbers));
+            }
+        }
+    }
+}
---
+++ Lists/Zadacha 1/Zadacha 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Zadacha_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            
+        }
+    }
+}
